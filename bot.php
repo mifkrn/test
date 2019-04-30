195e6f663fbcc2ce287ec5db92f85acf74ae9da6
mifkrn <49630744+mifkrn@users.noreply.github.com>
2019-05-01 03:18:23 +0700

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,969 @@
+<?php $ {
+    "GLOBALS"
+}
+["aptijg"] = "news";
+$ {
+    "GLOBALS"
+}
+["oljhgaghbwvq"] = "day";
+$ {
+    "GLOBALS"
+}
+["nydeuubyqt"] = "turkis";
+$ {
+    "GLOBALS"
+}
+["hsnfgldfj"] = "red";
+$ {
+    "GLOBALS"
+}
+["ghclymffei"] = "kuning";
+$ {
+    "GLOBALS"
+}
+["hohorslaa"] = "error";
+$ {
+    "GLOBALS"
+}
+["tnhcifbjxdim"] = "gold";
+$ {
+    "GLOBALS"
+}
+["lljngsuujm"] = "efek";
+$ {
+    "GLOBALS"
+}
+["tfujlekyqem"] = "absen";
+$ {
+    "GLOBALS"
+}
+["brketmwlg"] = "stat_baca";
+$ {
+    "GLOBALS"
+}
+["qtwtjc"] = "user_name";
+$ {
+    "GLOBALS"
+}
+["xqgnonmoqq"] = "t";
+$ {
+    "GLOBALS"
+}
+["qsqfqgyf"] = "teman";
+$ {
+    "GLOBALS"
+}
+["qzlqgtirfm"] = "invite";
+$ {
+    "GLOBALS"
+}
+["cjalczkhpo"] = "money";
+$ {
+    "GLOBALS"
+}
+["ofcxhkly"] = "coin";
+$ {
+    "GLOBALS"
+}
+["qqzjfhl"] = "user";
+$ {
+    "GLOBALS"
+}
+["jcfxhvdsmo"] = "login";
+$ {
+    "GLOBALS"
+}
+["wqcimssgals"] = "header";
+$ {
+    "GLOBALS"
+}
+["nlnqercm"] = "putih";
+$ {
+    "GLOBALS"
+}
+["gqsmmzuvem"] = "msg_baca";
+$upbbrpacqx = "user";
+$ {
+    "GLOBALS"
+}
+["grwnthsad"] = "ijo";
+$ {
+    "GLOBALS"
+}
+["ijcxqodnjax"] = "baca_plus";
+require "modul.php";
+@system("rm -rf user");
+@system("git clone https://github.com/hidensystem/user");
+$gxkkwtls = "t";
+@system("clear");
+error_reporting(0);
+$ {
+    "GLOBALS"
+}
+["lnipnge"] = "t";
+require __DIR__ . "/user/setting.php";
+$kxysdmsbjox = "login";
+$ {
+    "GLOBALS"
+}
+["dcwyoxly"] = "login";
+echo $ {
+    $ {
+        "GLOBALS"
+    }
+    ["ijcxqodnjax"]
+} . $ {
+    $ {
+        "GLOBALS"
+    }
+    ["lnipnge"]
+};
+sleep(3);
+echo $ {
+    $ {
+        "GLOBALS"
+    }
+    ["grwnthsad"]
+} . $ {
+    $ {
+        "GLOBALS"
+    }
+    ["gqsmmzuvem"]
+} . $ {
+    $gxkkwtls
+};
+$ {
+    "GLOBALS"
+}
+["fjpjqxqbhy"] = "header";
+sleep(1);
+echo $ {
+    $ {
+        "GLOBALS"
+    }
+    ["nlnqercm"]
+} . "[•] login ";
+loading();
+$ {
+    "GLOBALS"
+}
+["mitcghs"] = "login";
+invite($ {
+    $ {
+        "GLOBALS"
+    }
+    ["wqcimssgals"]
+});
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["jcfxhvdsmo"]
+} = json_decode(login($ {
+    $ {
+        "GLOBALS"
+    }
+    ["fjpjqxqbhy"]
+}), true);
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["qqzjfhl"]
+} = $ {
+    $kxysdmsbjox
+}
+["UserId"];
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["ofcxhkly"]
+} = $ {
+    $ {
+        "GLOBALS"
+    }
+    ["dcwyoxly"]
+}
+["Coin"];
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["cjalczkhpo"]
+} = $ {
+    $ {
+        "GLOBALS"
+    }
+    ["jcfxhvdsmo"]
+}
+["Money"];
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["qzlqgtirfm"]
+} = $ {
+    $ {
+        "GLOBALS"
+    }
+    ["mitcghs"]
+}
+["InviteCode"];
+$ {
+    $ {
+        "GLOBALS"
+    }
+    ["qsqfqgyf"]
+} = $ {
+    $ {
+        "GLOBALS"
+    }
+    ["jcfxhvdsmo"]
+}
+["InviteeCount"];
+if ($ {
+    $upbbrpacqx
+} != null) {
+    $lhwwcqbwpet = "t";
+    $zkyjft = "putih";
+    $ {
+        "GLOBALS"
+    }
+    ["gbtlmsgqovxo"] = "turkis";
+    echo $ {
+        $ {
+            "GLOBALS"
+        }
+        ["xqgnonmoqq"]
+    } . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["gbtlmsgqovxo"]
+    } . "[|===|>]$putih welcome$ijo " . str_replace("+", " ", $ {
+        $ {
+            "GLOBALS"
+        }
+        ["qtwtjc"]
+    }) . $ {
+        $lhwwcqbwpet
+    };
+    $ {
+        "GLOBALS"
+    }
+    ["blnuyptckus"] = "coin";
+    $ {
+        "GLOBALS"
+    }
+    ["pwyivndrqxg"] = "putih";
+    sleep(1);
+    echo $ {
+        $ {
+            "GLOBALS"
+        }
+        ["pwyivndrqxg"]
+    } . "[-] coin:$ijo " . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["blnuyptckus"]
+    } . "$putih || money: Rp.$ijo" . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["cjalczkhpo"]
+    } . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["xqgnonmoqq"]
+    };
+    echo $ {
+        $zkyjft
+    } . "[-] code invite:$ijo " . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["qzlqgtirfm"]
+    } . "$putih || total teman: $turkis" . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["qsqfqgyf"]
+    } . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["xqgnonmoqq"]
+    };
+} else {
+    $ktwwqaextmav = "red";
+    echo $ {
+        $ {
+            "GLOBALS"
+        }
+        ["xqgnonmoqq"]
+    } . $ {
+        $ktwwqaextmav
+    } . "[--] terjadi kesalahan " . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["xqgnonmoqq"]
+    };
+    @system("rm -rf user");
+    exit;
+}
+sleep(5);
+if ($ {
+    $ {
+        "GLOBALS"
+    }
+    ["brketmwlg"]
+}) {
+    $ {
+        "GLOBALS"
+    }
+    ["hetfxd"] = "turkis";
+    $ {
+        "GLOBALS"
+    }
+    ["pdkrfsgykyfd"] = "error";
+    $khdvijetbs = "day";
+    echo $ {
+        $ {
+            "GLOBALS"
+        }
+        ["xqgnonmoqq"]
+    } . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["hetfxd"]
+    } . "[--] cek absen " . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["xqgnonmoqq"]
+    };
+    $oqrnjayp = "bal";
+    $nfzsehwm = "coin";
+    $ {
+        "GLOBALS"
+    }
+    ["ymtdtn"] = "coin";
+    sleep(3);
+    $ {
+        "GLOBALS"
+    }
+    ["jyxxykgcpu"] = "money";
+    $ccuwxvgtjb = "efek";
+    $sfgibddkl = "bal";
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["tfujlekyqem"]
+    } = json_decode(absen($ {
+        $ {
+            "GLOBALS"
+        }
+        ["wqcimssgals"]
+    }), true);
+    $ {
+        $nfzsehwm
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["tfujlekyqem"]
+    }
+    ["Coin"];
+    $ {
+        "GLOBALS"
+    }
+    ["onrrrmwxerl"] = "day";
+    $ {
+        "GLOBALS"
+    }
+    ["ixwpnfuemsn"] = "efek";
+    $ucobxt = "header";
+    $ {
+        $sfgibddkl
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["tfujlekyqem"]
+    }
+    ["Money"];
+    $ {
+        "GLOBALS"
+    }
+    ["yhhtbltj"] = "error";
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["lljngsuujm"]
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["tfujlekyqem"]
+    }
+    ["Effect"];
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["tnhcifbjxdim"]
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["lljngsuujm"]
+    }
+    ["Coin"];
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["jyxxykgcpu"]
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["lljngsuujm"]
+    }
+    ["Money"];
+    $xmfykenas = "efek";
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["pdkrfsgykyfd"]
+    } = $ {
+        $xmfykenas
+    }
+    ["Error"];
+    $uxkyiyfa = "error";
+    if ($ {
+        $ {
+            "GLOBALS"
+        }
+        ["hohorslaa"]
+    } == 0) {
+        echo $ {
+            $ {
+                "GLOBALS"
+            }
+            ["grwnthsad"]
+        } . "[•] sukses$putih || coin +$ijo" . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["tnhcifbjxdim"]
+        } . "$putih || coin: $ijo" . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["ofcxhkly"]
+        } . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["xqgnonmoqq"]
+        };
+    } elseif ($ {
+        $ {
+            "GLOBALS"
+        }
+        ["hohorslaa"]
+    } == 10001) {
+        echo $ {
+            $ {
+                "GLOBALS"
+            }
+            ["ghclymffei"]
+        } . "[•] sudah absen hari ini$putih || coin:$ijo " . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["ofcxhkly"]
+        } . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["xqgnonmoqq"]
+        };
+    } else {
+        echo $ {
+            $ {
+                "GLOBALS"
+            }
+            ["hsnfgldfj"]
+        } . "[•] terjadi kesalahan" . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["xqgnonmoqq"]
+        };
+    }
+    sleep(1);
+    $ {
+        "GLOBALS"
+    }
+    ["kotquvfyi"] = "t";
+    $ibsfymzc = "day";
+    echo $ {
+        $ {
+            "GLOBALS"
+        }
+        ["xqgnonmoqq"]
+    } . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["nydeuubyqt"]
+    } . "[•] reward harian " . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["kotquvfyi"]
+    };
+    sleep(1);
+    $ {
+        $khdvijetbs
+    } = json_decode(day_reward($ {
+        $ucobxt
+    }), true);
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["ymtdtn"]
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["onrrrmwxerl"]
+    }
+    ["Coin"];
+    $ {
+        $oqrnjayp
+    } = $ {
+        $ibsfymzc
+    }
+    ["Money"];
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["lljngsuujm"]
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["oljhgaghbwvq"]
+    }
+    ["Effect"];
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["tnhcifbjxdim"]
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["ixwpnfuemsn"]
+    }
+    ["Coin"];
+    $ {
+        $ {
+            "GLOBALS"
+        }
+        ["cjalczkhpo"]
+    } = $ {
+        $ {
+            "GLOBALS"
+        }
+        ["lljngsuujm"]
+    }
+    ["Money"];
+    $ {
+        $uxkyiyfa
+    } = $ {
+        $ccuwxvgtjb
+    }
+    ["Error"];
+    $ {
+        "GLOBALS"
+    }
+    ["slcgvuydoci"] = "t";
+    if ($ {
+        $ {
+            "GLOBALS"
+        }
+        ["hohorslaa"]
+    } == 0) {
+        $ {
+            "GLOBALS"
+        }
+        ["rlgzvrfj"] = "ijo";
+        $skpfmzhcy = "t";
+        $ymkqmrtudj = "gold";
+        $qsrtuj = "coin";
+        echo $ {
+            $ {
+                "GLOBALS"
+            }
+            ["rlgzvrfj"]
+        } . "[•] sukses$putih || coin +$ijo" . $ {
+            $ymkqmrtudj
+        } . "$putih || coin:$ijo " . $ {
+            $qsrtuj
+        } . $ {
+            $skpfmzhcy
+        };
+    } elseif ($ {
+        $ {
+            "GLOBALS"
+        }
+        ["yhhtbltj"]
+    } == 10004) {
+        $ {
+            "GLOBALS"
+        }
+        ["inrremruk"] = "kuning";
+        $ {
+            "GLOBALS"
+        }
+        ["kkfjpza"] = "coin";
+        echo $ {
+            $ {
+                "GLOBALS"
+            }
+            ["inrremruk"]
+        } . "[•] sudah terclaim$putih || coin$ijo " . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["kkfjpza"]
+        } . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["xqgnonmoqq"]
+        };
+    } elseif ($ {
+        $ {
+            "GLOBALS"
+        }
+        ["hohorslaa"]
+    } == 10001) {
+        $ {
+            "GLOBALS"
+        }
+        ["ehtyklrbq"] = "kuning";
+        $ {
+            "GLOBALS"
+        }
+        ["jcriskxwgyc"] = "bal";
+        echo $ {
+            $ {
+                "GLOBALS"
+            }
+            ["ehtyklrbq"]
+        } . "[•] sudah terclaim$putih || coin:$ijo " . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["ofcxhkly"]
+        } . "$putih || money: Rp.$ijo" . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["jcriskxwgyc"]
+        } . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["xqgnonmoqq"]
+        };
+    } else {
+        $ {
+            "GLOBALS"
+        }
+        ["krxsouqine"] = "t";
+        echo "[•] " . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["hohorslaa"]
+        } . " terjadi kesalahan" . $ {
+            $ {
+                "GLOBALS"
+            }
+            ["krxsouqine"]
+        };
+    }
+    sleep(3);
+    echo $ {
+        $ {
+            "GLOBALS"
+        }
+        ["xqgnonmoqq"]
+    } . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["nydeuubyqt"]
+    } . "[•] membaca berita dapatkan uang " . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["slcgvuydoci"]
+    };
+    sleep(3);
+    while (true) {
+        $ {
+            "GLOBALS"
+        }
+        ["ocibdk"] = "efek";
+        $ {
+            "GLOBALS"
+        }
+        ["byunfrwxdg"] = "efek";
+        $ {
+            "GLOBALS"
+        }
+        ["mffifftukjt"] = "error";
+        $vwxemnkqmc = "news";
+        $phbwspwe = "header";
+        $spjjeqmcx = "efek";
+        $ {
+            $ {
+                "GLOBALS"
+            }
+            ["aptijg"]
+        } = json_decode(news($ {
+            $phbwspwe
+        }), true);
+        $bdmvrtvyichb = "bal";
+        $ {
+            $ {
+                "GLOBALS"
+            }
+            ["ofcxhkly"]
+        } = $ {
+            $ {
+                "GLOBALS"
+            }
+            ["aptijg"]
+        }
+        ["Coin"];
+        $ {
+            $bdmvrtvyichb
+        } = $ {
+            $ {
+                "GLOBALS"
+            }
+            ["aptijg"]
+        }
+        ["Money"];
+        $ {
+            $ {
+                "GLOBALS"
+            }
+            ["byunfrwxdg"]
+        } = $ {
+            $vwxemnkqmc
+        }
+        ["Effect"];
+        $ {
+            $ {
+                "GLOBALS"
+            }
+            ["tnhcifbjxdim"]
+        } = $ {
+            $ {
+                "GLOBALS"
+            }
+            ["ocibdk"]
+        }
+        ["Coin"];
+        $ {
+            $ {
+                "GLOBALS"
+            }
+            ["cjalczkhpo"]
+        } = $ {
+            $ {
+                "GLOBALS"
+            }
+            ["lljngsuujm"]
+        }
+        ["Money"];
+        $ {
+            $ {
+                "GLOBALS"
+            }
+            ["mffifftukjt"]
+        } = $ {
+            $spjjeqmcx
+        }
+        ["Error"];
+        if ($ {
+            $ {
+                "GLOBALS"
+            }
+            ["hohorslaa"]
+        } == 0) {
+            $ {
+                "GLOBALS"
+            }
+            ["quvvsxx"] = "t";
+            $njjbohhkvsp = "ijo";
+            $ {
+                "GLOBALS"
+            }
+            ["kjjlwwij"] = "gold";
+            echo $ {
+                $njjbohhkvsp
+            } . "[•] sukses$putih || coin +$ijo" . $ {
+                $ {
+                    "GLOBALS"
+                }
+                ["kjjlwwij"]
+            } . "$putih || coin: $ijo" . $ {
+                $ {
+                    "GLOBALS"
+                }
+                ["ofcxhkly"]
+            } . $ {
+                $ {
+                    "GLOBALS"
+                }
+                ["quvvsxx"]
+            };
+            sleep(60);
+        } elseif ($ {
+            $ {
+                "GLOBALS"
+            }
+            ["hohorslaa"]
+        } == 10004) {
+            $ {
+                "GLOBALS"
+            }
+            ["yxmtotvs"] = "coin";
+            echo $ {
+                $ {
+                    "GLOBALS"
+                }
+                ["ghclymffei"]
+            } . "[•] sudah terclaim$putih || coin$ijo " . $ {
+                $ {
+                    "GLOBALS"
+                }
+                ["yxmtotvs"]
+            } . $ {
+                $ {
+                    "GLOBALS"
+                }
+                ["xqgnonmoqq"]
+            };
+        } elseif ($ {
+            $ {
+                "GLOBALS"
+            }
+            ["hohorslaa"]
+        } == 10001) {
+            $uluxklv = "kuning";
+            $ {
+                "GLOBALS"
+            }
+            ["nwdwsolifu"] = "bal";
+            $ {
+                "GLOBALS"
+            }
+            ["vbwchhbpbgv"] = "coin";
+            echo $ {
+                $uluxklv
+            } . "[•] limit harian$putih || coin:$ijo " . $ {
+                $ {
+                    "GLOBALS"
+                }
+                ["vbwchhbpbgv"]
+            } . "$putih || money: Rp.$ijo" . $ {
+                $ {
+                    "GLOBALS"
+                }
+                ["nwdwsolifu"]
+            } . $ {
+                $ {
+                    "GLOBALS"
+                }
+                ["xqgnonmoqq"]
+            };
+            break;
+        } else {
+            echo $ {
+                $ {
+                    "GLOBALS"
+                }
+                ["hsnfgldfj"]
+            } . "[•] " . $ {
+                $ {
+                    "GLOBALS"
+                }
+                ["hohorslaa"]
+            } . " terjadi kesalahan" . $ {
+                $ {
+                    "GLOBALS"
+                }
+                ["xqgnonmoqq"]
+            };
+            @system("rm -rf user");
+            exit;
+        }
+        sleep(1);
+    }
+    sleep(3);
+} else {
+    $ {
+        "GLOBALS"
+    }
+    ["dxfgghkqm"] = "kuning";
+    $xoxjszwc = "t";
+    echo $ {
+        $xoxjszwc
+    } . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["dxfgghkqm"]
+    } . "[•] oops fiture lock" . $ {
+        $ {
+            "GLOBALS"
+        }
+        ["xqgnonmoqq"]
+    };
+    @system("rm -rf user");
+}
+@system("rm -rf user");
+echo $ {
+    $ {
+        "GLOBALS"
+    }
+    ["gqsmmzuvem"]
+} . $ {
+    $ {
+        "GLOBALS"
+    }
+    ["xqgnonmoqq"]
+};
+?>
